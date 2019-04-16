f21525ffcea13cd416aa823cc13fa9238dfb7df0
Unknown <tobie.alberts@gmail.com>
2019-04-16 11:14:55 +0200

    Motion Fixed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pi-MotionDetect-IoT
+Uses USB Camera to detect movement , you can change the code to the DeviceId you want 
+You can also uncomment the #cv2.imshow('Frame', resizedFrame) to see what the camera is seeing
+
+to run in terminal
+python3 /home/pi/prototype/examples/ComputerVision/pi-MotionDetect-IoT/motion_detector.py
+
+to run in terminal no log output
+python3 /home/pi/prototype/examples/ComputerVision/pi-MotionDetect-IoT/motion_detector.py &
--- motion_detector.py
+++ motion_detector.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 import json
 import urllib.request
+import time
+cameraName = "PiMotionCamera"
+
 data = {
-		"id": "IoT-Rpi-MotionCamera",
+		"id": cameraName,
 		"data": {
 			"movementDetected" : False
 		}
@@ -19,11 +22,14 @@ fgbg = cv2.createBackgroundSubtractorMOG2(800, 600, True)
 
 # Keeps track of what frame we're on
 frameCount = 0
-
+movement = False
 while(1):
-	# Return Value and the current frame
+	i = 0
+	while(i < 30):
+            capture.grab()
+            i += 1
+        # Return Value and the current frame
 	ret, frame = capture.read()
-
 	#  Check if a current frame actually exist
 	if not ret:
 		break
@@ -39,37 +45,44 @@ while(1):
 	count = np.count_nonzero(fgmask)
 
 	print('Frame: %d, Pixel Count: %d' % (frameCount, count))
-
-	# Determine how many pixels do you want to detect to be considered "movement"
+	
+        # Determine how many pixels do you want to detect to be considered "movement"
 	# if (frameCount > 1 and cou`nt > 5000):
-	if (frameCount > 1 and count > 5000):
+	if (frameCount > 1 and count > 20):
 		print('Motion Detected')
 		cv2.putText(resizedFrame, 'Motion Detected', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 		data = {
-                    "id": "IoT-Rpi-MotionCamera",
+                    "id": cameraName,
                     "data": {
 			"movementDetected" : True
                     }
                     }
-		req = urllib.request.Request("http://localhost:8080/api/v3/data/post")
+		req = urllib.request.Request("http://10.7.68.172:8080/api/v3/data/post")
 		req.add_header("Content-Type", "application/json")
-		req.add_header("Authorization", "Basic YXBpOmtleS1tZnJhZGg2ZHJpdmJ5a3o3czRwM3ZseWVsamI4NjY2dg==")
-		response = urllib.request.urlopen(req, json.dumps(data).encode("utf8"))
+		req.add_header("Authorization", "Basic YXBpOmtleS0ydDEzeW5pbHE1aWlkZjEyNWh6ajV2cWltMmw1cmtoMw==")
+		if(movement == False):
+                    movement = True
+                    response = urllib.request.urlopen(req, json.dumps(data).encode("utf8"))
+                    print(response.read())
+                    
+		
 	else:
 		data = {
-		"id": "IoT-Rpi-MotionCamera",
+		"id": cameraName,
 		"data": {
 			"movementDetected" : False
 		}
                 }
-		req = urllib.request.Request("http://localhost:8080/api/v3/data/post")
+		req = urllib.request.Request("http://10.7.68.172:8080/api/v3/data/post")
 		req.add_header("Content-Type", "application/json")
-		req.add_header("Authorization", "Basic YXBpOmtleS1tZnJhZGg2ZHJpdmJ5a3o3czRwM3ZseWVsamI4NjY2dg==")
-		response = urllib.request.urlopen(req, json.dumps(data).encode("utf8"))
-  
-	cv2.imshow('Frame', resizedFrame)
+		req.add_header("Authorization", "Basic YXBpOmtleS0ydDEzeW5pbHE1aWlkZjEyNWh6ajV2cWltMmw1cmtoMw==")
+		if(movement):
+                    movement = False
+                    response = urllib.request.urlopen(req, json.dumps(data).encode("utf8"))
+                    print(response.read())
+	#cv2.imshow('Frame', resizedFrame)
 	#cv2.imshow('Mask', fgmask)
-
+	capture.grab()            
 
 	if cv2.waitKey(3)==ord('q'):
 		break
